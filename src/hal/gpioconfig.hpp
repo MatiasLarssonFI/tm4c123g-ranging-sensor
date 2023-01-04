1a32e08283aec92bd4b9357b064410c2550ba942
Matias Larsson <matias.larsson@gmail.com>
2023-01-04 12:44:40 +0100

    Add file missed from previous commit

---
+++ src/hal/gpioconfig.hpp
@@ -0,0 +1,33 @@
+#ifndef GPIOCONFIG_CXX
+#define GPIOCONFIG_CXX
+
+#include <cstdint>
+
+#include "bsp/bsp.h"
+
+
+/*
+ * GPIO interrupt configurations.
+ * 
+ * Values for GPIO interrupt configuration registers.
+ */
+
+namespace {
+    struct GPIOIntSenseImpl {
+        enum class IS { EDGE, LEVEL };
+        IS is;
+    }
+
+
+    struct GPIOIntEventImpl {
+        enum class IEV { LOW, HIGH };
+        IEV iev;
+    }
+}
+
+namespace GPIOIntConfig {
+    using Sense = GPIOIntSenseImpl;
+    using Event = GPIOIntEventImpl;
+};
+
+#endif
